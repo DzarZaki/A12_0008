61e7f2820e7e63ba3aa36e83a8eec7cf887170ab
DzarZaki <dzarzaki2@gmail.com>
2025-01-19 20:57:22 +0700

    menambahkan model response untuk pendaftaran dan detail Pendaftaran

---
+++ app/src/main/java/com/example/finalpam/model/Pendaftaran.kt
@@ -0,0 +1,29 @@
+package com.example.finalpam.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AllPendaftaranResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Pendaftaran>
+)
+
+@Serializable
+data class PendaftaranDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Pendaftaran
+)
+
+@Serializable
+data class Pendaftaran(
+    val idPendaftaran: String,
+    @SerialName("id_siswa")
+    val idSiswa: String,
+    @SerialName("id_kursus")
+    val idKursus: String,
+    val tanggal: String,
+    val status: String
+)
