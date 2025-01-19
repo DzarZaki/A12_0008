3a5872cd8ae7a31a896f54cca9d1054b4d244c84
DzarZaki <dzarzaki2@gmail.com>
2025-01-19 21:33:32 +0700

    menambahkan interface InstrukturService untuk operasi CRUD instruktur

---
+++ app/src/main/java/com/example/finalpam/service_api/InstrukturService.kt
@@ -0,0 +1,40 @@
+package com.example.finalpam.service_api
+
+import com.example.finalpam.model.AllInstrukturResponse
+import com.example.finalpam.model.Instruktur
+import com.example.finalpam.model.InstrukturDetailResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface InstrukturService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+
+    @POST("instruktur")
+    suspend fun insertInstruktur(@Body instruktur: Instruktur) : retrofit2.Response<Void>
+
+
+    @GET("instruktur")
+    suspend fun getAllInstruktur(): AllInstrukturResponse
+
+    @GET("instruktur/{id_instruktur}")
+    suspend fun getInstrukturById(@Path("id_instruktur") idInstruktur: String): InstrukturDetailResponse
+
+    @PUT("instruktur/{id_instruktur}")
+    suspend fun updateInstruktur(
+        @Path("id_instruktur") idInstruktur: String,
+        @Body instruktur: Instruktur
+    ) : retrofit2.Response<Void>
+
+
+    @DELETE("instruktur/{id_instruktur}")
+    suspend fun deleteInstruktur(@Path("id_instruktur") idInstruktur: String) : retrofit2.Response<Void>
+
+}
