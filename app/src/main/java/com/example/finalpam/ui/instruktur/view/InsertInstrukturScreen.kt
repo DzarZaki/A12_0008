1dae1fa7032b41d46bc901cb0bded06f66875509
DzarZaki <dzarzaki2@gmail.com>
2025-01-22 20:32:44 +0700

    menambahkan InsertInstrukturScreen untuk menambah data instruktur baru

---
+++ app/src/main/java/com/example/finalpam/ui/instruktur/view/InsertInstrukturScreen.kt
@@ -0,0 +1,142 @@
+package com.example.finalpam.ui.instruktur.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalpam.PenyediaViewModel
+import com.example.finalpam.ui.costumwidget.CustomTopAppBar
+import com.example.finalpam.ui.instruktur.viewmodel.InsertInstrukturUiEvent
+import com.example.finalpam.ui.instruktur.viewmodel.InsertInstrukturUiState
+import com.example.finalpam.ui.instruktur.viewmodel.InsertInstrukturViewModel
+import com.example.finalpam.ui.navigation.DestinasiNavigasi
+import kotlinx.coroutines.launch
+
+object DestinasiInstrukturEntry : DestinasiNavigasi {
+    override val route = "instruktur_entry"
+    override val titleRes = "Tambah Instruktur"
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertInstrukturScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertInstrukturViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = "Tambah Instruktur",
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onInstrukturValueChange = viewModel::updateInsertInstrukturState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertInstruktur()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertInstrukturUiState,
+    onInstrukturValueChange: (InsertInstrukturUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onInstrukturValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@Composable
+fun FormInput(
+    insertUiEvent: InsertInstrukturUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertInstrukturUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = { onValueChange(insertUiEvent.copy(nama = it)) },
+            label = { Text(text = "Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.email,
+            onValueChange = { onValueChange(insertUiEvent.copy(email = it)) },
+            label = { Text(text = "Email") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.nomorTelepon,
+            onValueChange = { onValueChange(insertUiEvent.copy(nomorTelepon = it)) },
+            label = { Text(text = "Nomor Telepon") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.deskripsi,
+            onValueChange = { onValueChange(insertUiEvent.copy(deskripsi = it)) },
+            label = { Text(text = "Deskripsi") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+    }
+}
