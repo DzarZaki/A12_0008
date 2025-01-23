50a9276cc0bed9e626efd139e002f7ff363d7e2a
DzarZaki <dzarzaki2@gmail.com>
2025-01-24 06:46:07 +0700

    menambahkan UpdatePendaftaranViewModel untuk mengelola pembaruan data pendaftaran

---
+++ app/src/main/java/com/example/finalpam/ui/pendaftaran/viewmodel/UpdatePendaftaranViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.finalpam.ui.pendaftaran.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalpam.repository.PendaftaranRepository
+import kotlinx.coroutines.launch
+
+class UpdatePendaftaranViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val pendaftaranRepository: PendaftaranRepository
+) : ViewModel() {
+
+    var updateUIState by mutableStateOf(InsertPendaftaranUiState())
+        private set
+
+    private val _idPendaftaran: String = checkNotNull(savedStateHandle[DestinasiUpdatePendaftaran.ID_PENDAFTARAN])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = pendaftaranRepository.getPendaftaranById(_idPendaftaran).data
+                .toUiStatePendaftaran()
+        }
+    }
+
+    fun updateInsertPendaftaranState(insertPendaftaranUiEvent: InsertPendaftaranUiEvent) {
+        updateUIState = InsertPendaftaranUiState(insertUiEvent = insertPendaftaranUiEvent)
+    }
+
+    fun updateData() {
+        viewModelScope.launch {
+            try {
+                pendaftaranRepository.updatePendaftaran(
+                    _idPendaftaran,
+                    updateUIState.insertUiEvent.toPendaftaran()
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
