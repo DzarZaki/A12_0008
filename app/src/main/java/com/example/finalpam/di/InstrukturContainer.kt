d2d1058940563da1f6603b8dee9162344745b0c2
DzarZaki <dzarzaki2@gmail.com>
2025-01-26 07:27:33 +0700

    menambahkan InstrukturContainer untuk menyediakan konfigurasi API dan repository instruktur

---
+++ app/src/main/java/com/example/finalpam/di/InstrukturContainer.kt
@@ -0,0 +1,30 @@
+package com.example.finalpam.di
+
+import InstrukturService
+import com.example.finalpam.repository.InstrukturRepository
+import com.example.finalpam.repository.NetworkInstrukturRepository
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface InstrukturAppContainer {
+    val instrukturRepository: InstrukturRepository
+}
+
+class InstrukturContainer : InstrukturAppContainer {
+
+    private val baseUrl = "http://10.0.2.2:80/umyTI/" // localhost diganti IP jika run di HP
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val instrukturService: InstrukturService by lazy {
+        retrofit.create(InstrukturService::class.java)
+    }
+
+    override val instrukturRepository: InstrukturRepository by lazy {
+        NetworkInstrukturRepository(instrukturService)
+    }
+}
