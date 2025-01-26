5a7463980852e393758a7e66f6696b701bc61ba3
DzarZaki <dzarzaki2@gmail.com>
2025-01-26 07:47:53 +0700

    Menambahkan container untuk Instruktur, Kursus, Pendaftaran, dan Siswa di FinalApplications

--- app/src/main/java/com/example/finalpam/FinalApplications.kt
+++ app/src/main/java/com/example/finalpam/FinalApplications.kt
@@ -1,14 +1,26 @@
 package com.example.finalpam
 
 import android.app.Application
-import com.example.finalpam.di.AppContainer
-import com.example.finalpam.di.MahasiswaContainer
+import com.example.finalpam.di.InstrukturAppContainer
+import com.example.finalpam.di.InstrukturContainer
+import com.example.finalpam.di.KursusAppContainer
+import com.example.finalpam.di.KursusContainer
+import com.example.finalpam.di.PendaftaranAppContainer
+import com.example.finalpam.di.PendaftaranContainer
+import com.example.finalpam.di.SiswaAppContainer
+import com.example.finalpam.di.SiswaContainer
 
 class FinalApplications : Application() {
-    lateinit var container: AppContainer
+    lateinit var instrukturContainer: InstrukturAppContainer
+    lateinit var kursusContainer: KursusAppContainer
+    lateinit var pendaftaranContainer: PendaftaranAppContainer
+    lateinit var siswaContainer: SiswaAppContainer
 
     override fun onCreate() {
         super.onCreate()
-        container = MahasiswaContainer()
+        instrukturContainer = InstrukturContainer()
+        kursusContainer = KursusContainer()
+        pendaftaranContainer = PendaftaranContainer()
+        siswaContainer = SiswaContainer()
     }
 }
